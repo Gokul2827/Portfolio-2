241e56ba9d29f95ff4c6d321289ff7505f772d7d
Gokul2827 <gokulakrishnana4@gmail.com>
2024-01-21 21:17:32 +0530

    new portfolio

---
+++ script2.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2500,
+    reset:true
+});
+
+sr.reveal('.Portfolio-heading',{delay:200, origin:'top'});
+sr.reveal('.Portfolio-content',{delay:400, origin:'left'});
+sr.reveal('.Portfolio-content2',{delay:520, origin:'right'});
+sr.reveal('.Portfolio-content3',{delay:620, origin:'left'});
+sr.reveal('.Portfolio-content4',{delay:700, origin:'right'});
+sr.reveal('.more',{delay:750, origin:'bottom'});
