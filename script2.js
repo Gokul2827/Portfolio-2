9b15c3a573453883cab474c82057a180160f9aba
Gokul2827 <gokulakrishnana4@gmail.com>
2024-01-29 13:20:24 +0530

    second change

--- script2.js
+++ script2.js
@@ -9,4 +9,6 @@ sr.reveal('.Portfolio-content',{delay:400, origin:'left'});
 sr.reveal('.Portfolio-content2',{delay:520, origin:'right'});
 sr.reveal('.Portfolio-content3',{delay:620, origin:'left'});
 sr.reveal('.Portfolio-content4',{delay:700, origin:'right'});
-sr.reveal('.more',{delay:750, origin:'bottom'});
+sr.reveal('.Portfolio-content5',{delay:780, origin:'left'});
+sr.reveal('.Portfolio-content6',{delay:820, origin:'right'});
+sr.reveal('.more',{delay:850, origin:'bottom'});
